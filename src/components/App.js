a8102fafe670861016e5768939c39192b7b1772b
Pedro Teixeira <i@pgte.me>
2017-10-06 14:30:32 +0300

    using hash router instead of browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Home from './Home'
 import Edit from './Edit'
