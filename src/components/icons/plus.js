9e6be8afaf3e1b0b303c4db15eb7d658fe346df9
Alan Shaw <alan@tableflip.io>
2017-10-17 12:42:17 +0100

    Adds header icons

---
+++ src/components/icons/plus.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ({ className, style }) => (
+  <svg width='14' height='14' viewBox='0 0 14 14' xmlns='http://www.w3.org/2000/svg' className={className} style={style}>
+    <g transform='translate(-1.000000, 0.000000)' strokeWidth='2' strokeLinecap='square'>
+      <path d='M8,0 L8,14' />
+      <path d='M0.5,7 L14.5,7' />
+    </g>
+  </svg>
+)
---
+++ src/components/icons/user.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ({ className, style }) => (
+  <svg width='14' height='14' viewBox='0 0 14 14' xmlns='http://www.w3.org/2000/svg' className={className} style={style}>
+    <g transform='translate(1.000000, 1.000000)' strokeWidth='1' fill='none'>
+      <circle cx='6' cy='3' r='3' />
+      <path d='M0.5,11.5 C0.5,11.5 0.586018855,9.26675755 2.03662109,7.66162109 C3.48722333,6.05648464 5.25696067,5.92724609 6,5.92724609 C6.87776618,5.92724609 9.08627,6.31034213 10.2138672,7.66162109 C11.3414644,9.01290006 11.5,11.5 11.5,11.5' stroke-linecap='round' />
+    </g>
+  </svg>
+)
