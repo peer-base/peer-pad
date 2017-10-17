79c176ac2a93364a5e1f23d14d0198e4490fc482
Alan Shaw <alan@tableflip.io>
2017-10-17 16:44:47 +0100

    Fixes react property warnings

--- src/components/icons/code.js
+++ src/components/icons/code.js
@@ -4,8 +4,8 @@ export default ({ className, style }) => (
   <svg width='29' height='29' viewBox='0 0 29 29' xmlns='http://www.w3.org/2000/svg' className={className} style={style}>
     <g transform='translate(8.000000, 10.000000)' strokeWidth='1' strokeLinecap='square'>
       <path d='M8.5,0.5 L5.5,9.5' />
-      <polyline stroke-linejoin='round' points='2.97436523 2.05053711 0.125976562 5.01806641 2.98657227 8.11206055' />
-      <polyline stroke-linejoin='round' transform='translate(12.500000, 5.030762) scale(-1, 1) translate(-12.500000, -5.030762) ' points='13.9871981 2 11 4.9675293 14 8.06152344' />
+      <polyline strokeLinejoin='round' points='2.97436523 2.05053711 0.125976562 5.01806641 2.98657227 8.11206055' />
+      <polyline strokeLinejoin='round' transform='translate(12.500000, 5.030762) scale(-1, 1) translate(-12.500000, -5.030762) ' points='13.9871981 2 11 4.9675293 14 8.06152344' />
     </g>
   </svg>
 )
