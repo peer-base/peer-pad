33d621a31e3e8f8724dea33648bb1bb698f0bf43
Oli Evans <oli@zilla.org.uk>
2017-10-25 15:28:06 +0100

    Remove the close button from the homepage warning

--- src/components/home/Warning.js
+++ src/components/home/Warning.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react'
 import { CloseIcon, AlphaIcon } from '../icons'
 
 export const Warning = ({onClose}) => (
-  <div className='bg-razzmatazz white' style={{padding: '10px 15px'}}>
+  <div className='bg-razzmatazz white' style={{padding: '15px'}}>
     <AlphaIcon className='dib fill--current-color v-mid mr3' />
     <span className='dib-ns v-mid f6 fw6 '>Peerpad is in Alpha. </span>
     <span className='db dib-ns pt3 pt0-l v-mid f6 fw3'>
       The codebase hasn't been audited by Security specialists and it shouldn't be used to store, share or publish sensitive information.
     </span>
-    <button
-      type='button'
-      className='db dib-ns w-100 w-auto-ns tc ml3-ns pv3 p0-ns bg-transparent b--none  white v-mid pointer'
-      onClick={onClose} >
-      <span className='dn-l v-mid f7 fw3'>Close </span>
-      <CloseIcon className='fill--current-color v-mid' />
-    </button>
+    {onClose ? (
+      <button
+        type='button'
+        className='db dib-ns w-100 w-auto-ns tc ml3-ns pv2 p0-ns bg-transparent b--none  white v-mid pointer'
+        onClick={onClose} >
+        <span className='dn-l v-mid f7 fw3'>Close </span>
+        <CloseIcon className='fill--current-color v-mid' />
+      </button>
+    ) : null }
   </div>
 )
 
-class WarningContainer extends Component {
+export class WarningContainer extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -49,4 +51,7 @@ class WarningContainer extends Component {
   }
 }
 
-export default WarningContainer
+// The Warning should not be dismissable yet...
+// see: https://github.com/ipfs-shipyard/peerpad/issues/59
+// Export WarningContainer when we're ready for dismissable warnings.
+export default Warning
