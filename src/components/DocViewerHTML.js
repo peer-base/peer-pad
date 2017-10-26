12deded74de789837fbdfb91062e2ccd4fe3ed36
Oli Evans <oli@zilla.org.uk>
2017-10-26 14:23:28 +0100

    Improved doc styling
    Styles are shared between in app preview and published snapshot

--- src/components/DocViewerHTML.js
+++ src/components/DocViewerHTML.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 // Hack to minimize source code size of doc snapshot..
 import parseSymmetricalKey from 'peerpad-core/src/backend/keys/parse-symm-key'
+import Doc from './Doc'
 
 class DocViewerHTML extends Component {
-
   constructor (props) {
     super(props)
     this.state = {
@@ -14,11 +14,18 @@ class DocViewerHTML extends Component {
   }
 
   render () {
-    const error = this.state.error && (<p class="error">Error: {this.state.error}</p>)
+    const {
+      error,
+      doc
+    } = this.state
+
     return (
       <div>
-        {error}
-        <div id="doc" dangerouslySetInnerHTML={{__html: (!this.state.error && this.state.doc) || '' }} />
+        {error ? (
+          <p>Error: {this.state.error}</p>
+        ) : (
+          <Doc html={doc} />
+        )}
       </div>
     )
   }
@@ -30,11 +37,11 @@ class DocViewerHTML extends Component {
         if (err) {
           this.setState({error: err.message})
         } else {
-          this.setState({ doc: decrypted.toString('utf8') })
+          this.setState({doc: decrypted.toString('utf8')})
         }
       })
     } catch (err) {
-      this.setState({error: err.message })
+      this.setState({error: err.message})
     }
   }
 }
