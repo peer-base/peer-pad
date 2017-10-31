9951c93f3f9215ba3916770c13dd13928f6d13d5
Pedro Teixeira <i@pgte.me>
2017-10-31 10:10:21 +0000

    snapshot and link icons. Fixes #82.

--- src/components/icons/snapshot.js
+++ src/components/icons/snapshot.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 export default ({ className, style }) => (
-  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 149 146.7' className={className} style={style}>
-    <path d='M85.86,41.35l5.56,8.44,2.31,3.56H110a4.12,4.12,0,0,1,4,4.13v43.94a3.93,3.93,0,0,1-4,3.94H38a4,4,0,0,1-4-4V57.41a4,4,0,0,1,4-4.06H54.28l2.38-3.56,5.62-8.44H86m4-8H58l-8,12H38A12.05,12.05,0,0,0,26,57.41v43.94a12,12,0,0,0,12,12h72a11.94,11.94,0,0,0,12-11.94V57.47a12.1,12.1,0,0,0-12-12.12H98l-8-12Z' />
-    <path d='M74,61.35a12,12,0,1,1-12,12,12,12,0,0,1,12-12m0-8a20,20,0,1,0,20,20,20,20,0,0,0-20-20Z' />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 149 146.7" className={className} style={style}>
+    <path d="M59.58,35H45.75c-7.58.12-9.35,1.89-9.79,9.17V59.48"/>
+    <path d="M113,58.66V44.84c-.11-7.58-1.88-9.35-9.16-9.8H88.61"/>
+    <path d="M36,87.22v15.27c.44,7.28,2.21,9.05,9.79,9.16H59.58"/>
+    <path d="M88.61,111.65h15.27c7.28-.44,9.05-2.21,9.16-9.79V88"/>
+    <line style={{ fill: 'none', stroke: 'rgb(181, 197, 223)', strokeLinecap: 'round', strokeMiterlimit: 10, strokeWidth: '8px'}} x1="74.5" y1="59.71" x2="74.5" y2="86.99"/>
+    <line style={{ fill: 'none', stroke: 'rgb(181, 197, 223)', strokeLinecap: 'round', strokeMiterlimit: 10, strokeWidth: '8px'}} x1="88.14" y1="73.35" x2="60.86" y2="73.35"/>
   </svg>
 )
