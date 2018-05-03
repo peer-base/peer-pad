9080402e048e39eb6014f29f3da52bfee26b7e71
Oli Evans <oli@tableflip.io>
2018-05-03 10:23:07 +0100

    (wip): Test syncing a pad between 2 peers

--- test/e2e/smoke.test.js
+++ test/e2e/smoke.test.js
@@ -1,47 +1,61 @@
-/* global it beforeAll afterAll expect */
+/* global it afterEach expect */
 
 import puppeteer from 'puppeteer'
 
-let browser = null
+let browsers = []
 
-// depends on a url that'll serve the app.
+async function createBrowser () {
+  const browser = await puppeteer.launch({headless: true})
+  browsers.push(browser)
+  return browser
+}
 
-beforeAll(async () => {
-  browser = await puppeteer.launch({headless: false, sloMo: 50})
+afterEach(() => {
+  browsers.forEach(b => b.close())
 })
 
-afterAll(() => {
-  browser.close()
-})
-
-it('Renders without errors in the browser', async (done) => {
+it('Renders without errors in the browser', async () => {
   const user = await createTabForUser('user')
   const errors = []
   user.on('error', err => errors.push(err))
   user.on('pageerror', err => errors.push(err))
   await createNewPad(user)
-  const peerId = findPeerId(user)
+  const peerId = await findPeerId(user)
   console.log('peerId', peerId)
+  expect(peerId).toBeTruthy()
   expect(peerId.length).toBeGreaterThan(10)
   expect(errors.length).toBe(0)
-})
+}, 30000)
 
 it('synchronises two pads via ipfs', async (done) => {
   const alf = await createTabForUser('alf')
   await createNewPad(alf)
+  const alfPeerId = await findPeerId(alf)
+  expect(alfPeerId).toBeTruthy()
   const alfTitle = 'I R ROBOT'
   await alf.type('[data-id=document-title-input]', alfTitle)
 
   const bob = await createTabForUser('bob')
+  console.log('pad url', alf.url())
   await bob.goto(alf.url())
+  const bobPeerId = await findPeerId(bob)
+  expect(bobPeerId).toBeTruthy()
+  expect(bobPeerId).not.toEqual(alfPeerId)
+  console.log('alf peerId', alfPeerId)
+  console.log('bob peerId', bobPeerId)
   setTimeout(async () => {
-    const bobTitle = await getDocumentTitle(bob)
-    expect(bobTitle).toEqual(alfTitle)
+    try {
+      const bobTitle = await getDocumentTitle(bob)
+      expect(bobTitle).toEqual(alfTitle)
+    } catch (err) {
+      done.fail(err)
+    }
     done()
-  }, 10000)
-}, 60000)
+  }, 5000)
+}, 3 * 60 * 1000)
 
 async function getDocumentTitle (page) {
+  await page.waitForSelector('[data-id=document-title-input]')
   const titleEl = await page.$('[data-id=document-title-input]')
   return page.evaluate(el => el.value, titleEl)
 }
@@ -62,6 +76,7 @@ async function findPeerId (page) {
 }
 
 async function createTabForUser (username) {
+  const browser = await createBrowser()
   const page = await browser.newPage()
   // page.on('console', msg => console.log(`${username} console:`, msg.text()))
   return page
