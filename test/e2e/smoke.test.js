dbd20c770f24821ec3791be766f6aea674803772
Oli Evans <oli@tableflip.io>
2018-05-03 13:17:12 +0100

    (wip): run e2e test on travis

--- test/e2e/smoke.test.js
+++ test/e2e/smoke.test.js
@@ -1,11 +1,23 @@
 /* global it afterEach expect */
-
 import puppeteer from 'puppeteer'
+import ms from 'milliseconds'
 
+// Which deployment to run the tests against
+const appUrl = process.env.URL || 'http://localhost:3000'
+const debug = process.env.DEBUG === 'true'
+
+console.log('Testing', appUrl)
+
+// puppeteer.launch opts
+// see: https://github.com/GoogleChrome/puppeteer/blob/v1.3.0/docs/api.md#puppeteerlaunchoptions
+const headless = !debug
+const slowMo = debug ? 100 : undefined
+
+// track browser instances so we can close them all after the test.
 let browsers = []
 
 async function createBrowser () {
-  const browser = await puppeteer.launch({headless: true})
+  const browser = await puppeteer.launch({headless, slowMo})
   browsers.push(browser)
   return browser
 }
@@ -14,45 +26,56 @@ afterEach(() => {
   browsers.forEach(b => b.close())
 })
 
-it('Renders without errors in the browser', async () => {
-  const user = await createTabForUser('user')
-  const errors = []
-  user.on('error', err => errors.push(err))
-  user.on('pageerror', err => errors.push(err))
-  await createNewPad(user)
-  const peerId = await findPeerId(user)
-  console.log('peerId', peerId)
-  expect(peerId).toBeTruthy()
-  expect(peerId.length).toBeGreaterThan(10)
-  expect(errors.length).toBe(0)
-}, 30000)
+it('Create a pad', async (done) => {
+  try {
+    const user = await createTabForUser('user')
+    const errors = []
+    user.on('error', err => errors.push(err))
+    user.on('pageerror', err => errors.push(err))
+    await createNewPad(user)
+    const peerId = await findPeerId(user)
+    console.log('peerId', peerId)
+    expect(peerId).toBeTruthy()
+    expect(peerId.length).toBeGreaterThan(10)
+    expect(errors.length).toBe(0)
+  } catch (err) {
+    return done.fail(err)
+  }
+  done()
+}, ms.seconds(30))
 
-it('synchronises two pads via ipfs', async (done) => {
-  const alf = await createTabForUser('alf')
-  await createNewPad(alf)
-  const alfPeerId = await findPeerId(alf)
-  expect(alfPeerId).toBeTruthy()
-  const alfTitle = 'I R ROBOT'
-  await alf.type('[data-id=document-title-input]', alfTitle)
+it('synchronises two pads via IPFS', async (done) => {
+  try {
+    const alf = await createTabForUser('alf')
+    await createNewPad(alf)
+    const alfPeerId = await findPeerId(alf)
+    expect(alfPeerId).toBeTruthy()
+    const alfTitle = 'I R ROBOT'
+    await alf.type('[data-id=document-title-input]', alfTitle)
 
-  const bob = await createTabForUser('bob')
-  console.log('pad url', alf.url())
-  await bob.goto(alf.url())
-  const bobPeerId = await findPeerId(bob)
-  expect(bobPeerId).toBeTruthy()
-  expect(bobPeerId).not.toEqual(alfPeerId)
-  console.log('alf peerId', alfPeerId)
-  console.log('bob peerId', bobPeerId)
-  setTimeout(async () => {
-    try {
-      const bobTitle = await getDocumentTitle(bob)
-      expect(bobTitle).toEqual(alfTitle)
-    } catch (err) {
-      done.fail(err)
-    }
-    done()
-  }, 5000)
-}, 3 * 60 * 1000)
+    const bob = await createTabForUser('bob')
+    console.log('pad url', alf.url())
+    await bob.goto(alf.url())
+    const bobPeerId = await findPeerId(bob)
+    expect(bobPeerId).toBeTruthy()
+    expect(bobPeerId).not.toEqual(alfPeerId)
+    console.log('alf peerId', alfPeerId)
+    console.log('bob peerId', bobPeerId)
+    // wait for ifps to sync...
+    const waitFor = ms.seconds(5)
+    setTimeout(async () => {
+      try {
+        const bobTitle = await getDocumentTitle(bob)
+        expect(bobTitle).toEqual(alfTitle)
+      } catch (err) {
+        done.fail(err)
+      }
+      done()
+    }, waitFor)
+  } catch (err) {
+    done.fail(err)
+  }
+}, ms.minutes(3)) // spinning up 2 browsers and syncing pads can take a while.
 
 async function getDocumentTitle (page) {
   await page.waitForSelector('[data-id=document-title-input]')
@@ -61,11 +84,10 @@ async function getDocumentTitle (page) {
 }
 
 async function createNewPad (page) {
-  await page.goto('http://localhost:3000')
-  await page.waitForSelector('[data-id=start-button]')
+  await page.goto(appUrl)
+  await page.waitForSelector('[data-id=start-button]', { timeout: ms.seconds(5) }) // start button should appear quickly, so we lower the timeout here
   await page.click('[data-id=start-button]')
   await page.waitForSelector('[data-id=ipfs-status][data-value=online]')
-  return page
 }
 
 async function findPeerId (page) {
@@ -78,6 +100,8 @@ async function findPeerId (page) {
 async function createTabForUser (username) {
   const browser = await createBrowser()
   const page = await browser.newPage()
+  // See: https://github.com/GoogleChrome/puppeteer/issues/1183#issuecomment-383722137
+  await page._client.send('Emulation.clearDeviceMetricsOverride')
   // page.on('console', msg => console.log(`${username} console:`, msg.text()))
   return page
 }
