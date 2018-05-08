5c8413088e497b0fa69d40542d511caa735dd472
Oli Evans <oli@tableflip.io>
2018-05-08 15:17:28 +0100

    (fix): Catch 404s in e2e test (#158)
    - Check requests to see if any result in a >=400 status code
    - waitFor the doc titles to match rather than waiting for 5s

--- test/e2e/smoke.test.js
+++ test/e2e/smoke.test.js
@@ -28,50 +28,66 @@ afterEach(() => {
   browsers.forEach(b => b.close())
 })
 
-it('Create a pad', async () => {
-  const user = await createTabForUser('user')
+it('creates a pad', async () => {
+  const page = await createPage()
+
+  // Catch any errors
   const errors = []
-  user.on('error', err => errors.push(err))
-  user.on('pageerror', err => errors.push(err))
-  await createNewPad(user)
-  const peerId = await findPeerId(user)
+  page.on('error', err => errors.push(err.message)) // Emitted when the page crashes.
+  page.on('pageerror', err => errors.push(err.message)) // Emitted when an uncaught exception happens within the page.
+  page.on('requestfailed', req => {
+    errors.push(`${req.failure().errorText} ${req.url()}`)
+  })
+  page.on('response', res => {
+    if (res.status() >= 400) {
+      errors.push(`${res.status()} ${res.url()}`)
+    }
+  })
+
+  // Create a pad and wait for IPFS to init.
+  await createNewPad(page)
+  const peerId = await waitForPeerId(page)
   console.log('peerId', peerId)
+
   expect(peerId).toBeTruthy()
   expect(peerId.length).toBeGreaterThan(10)
+  errors.forEach(err => console.log(err))
   expect(errors.length).toBe(0)
 }, ms.minutes(3))
 
 it('synchronises two pads via IPFS', async () => {
-  const alf = await createTabForUser('alf')
+  const docTitleSelector = '[data-id=document-title-input]'
+
+  const alf = await createPage()
   await createNewPad(alf)
-  const alfPeerId = await findPeerId(alf)
+  const alfPeerId = await waitForPeerId(alf)
+  console.log('alf peerId', alfPeerId)
   expect(alfPeerId).toBeTruthy()
   const alfTitle = 'I R ROBOT'
-  await alf.type('[data-id=document-title-input]', alfTitle)
+  await alf.type(docTitleSelector, alfTitle)
+  const padUrl = alf.url()
+  console.log('pad url', padUrl)
 
-  const bob = await createTabForUser('bob')
-  console.log('pad url', alf.url())
-  await bob.goto(alf.url())
-  const bobPeerId = await findPeerId(bob)
+  const bob = await createPage()
+  await bob.goto(padUrl)
+  const bobPeerId = await waitForPeerId(bob)
   expect(bobPeerId).toBeTruthy()
   expect(bobPeerId).not.toEqual(alfPeerId)
-  console.log('alf peerId', alfPeerId)
   console.log('bob peerId', bobPeerId)
 
-  // wait for IPFS to sync...
-  await pause(ms.seconds(5))
-  const bobTitle = await getDocumentTitle(bob)
-  expect(bobTitle).toEqual(alfTitle)
+  // Wait for bob's doc title to match alf's
+  const bobTitleRef = await bob.$(docTitleSelector)
+  const valueMatches = (input, expected) => input.value === expected
+  // waitFor fn is executed in the browser context, so context has to passed as args.
+  await bob.waitFor(valueMatches, {/* opts */}, bobTitleRef, alfTitle)
 }, ms.minutes(6)) // spinning up 2 browsers and syncing pads can take a while.
 
-function pause (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-async function getDocumentTitle (page) {
-  await page.waitForSelector('[data-id=document-title-input]')
-  const titleEl = await page.$('[data-id=document-title-input]')
-  return page.evaluate(el => el.value, titleEl)
+async function createPage () {
+  const browser = await createBrowser()
+  const page = await browser.newPage()
+  // See: https://github.com/GoogleChrome/puppeteer/issues/1183#issuecomment-383722137
+  await page._client.send('Emulation.clearDeviceMetricsOverride')
+  return page
 }
 
 async function createNewPad (page) {
@@ -82,19 +98,10 @@ async function createNewPad (page) {
   await page.waitForSelector('[data-id=ipfs-status][data-value=online]', {timeout: ms.minutes(2)})
 }
 
-async function findPeerId (page) {
+async function waitForPeerId (page) {
   // wait for IPFS to boot
   await page.waitForSelector('[data-peer-id]', {timeout: ms.minutes(2)})
   const peersButton = await page.$('[data-peer-id]')
   const peerId = await page.evaluate(el => el.dataset.peerId, peersButton)
   return peerId
-}
-
-async function createTabForUser (username) {
-  const browser = await createBrowser()
-  const page = await browser.newPage()
-  // See: https://github.com/GoogleChrome/puppeteer/issues/1183#issuecomment-383722137
-  await page._client.send('Emulation.clearDeviceMetricsOverride')
-  // page.on('console', msg => console.log(`${username} console:`, msg.text()))
-  return page
 }
