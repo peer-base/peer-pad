5f0792a676767ca8c0858b632130bac7fa46b6b0
Pedro Teixeira <i@pgte.me>
2018-10-23 18:55:00 +0100

    add text behaviour

--- test/e2e-load/replica.js
+++ test/e2e-load/replica.js
@@ -2,7 +2,7 @@
 
 const ms = require('milliseconds')
 const injectConfig = require('./inject-config')
-const replicaBehavior = require('./replica-behavior')
+const replicaAddTextBehavior = require('./replica-add-text-behavior')
 
 module.exports = ({events, text}) => async ({ page, data: url, worker }) => {
   try {
@@ -11,7 +11,7 @@ module.exports = ({events, text}) => async ({ page, data: url, worker }) => {
     await page.goto(url)
     page.on('console', (m) => events.emit('message', `[worker ${worker.id}]: ${m.text()}`))
     await page.waitForSelector('[data-id=ipfs-status][data-value=online]', {timeout: ms.minutes(2)})
-    await replicaBehavior({page, worker, text})
+    await replicaAddTextBehavior({page, worker, text})
   } catch (err) {
     console.error(`error in worker ${worker.id}:`, err)
     throw err
