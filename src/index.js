bd158d06e0492125023eb276af1718ad02b788f9
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-11-08 03:58:58 -0500

    feat: add error reporting via rollbar
    Adds reporting of errors when they happen on peerpad.net. We should
    figure out if we want this to be opt-in or opt-out. Currently it's
    neither and always enabled if peerpad is loaded via `peerpad.net`.
    
    Relevant people have received an invitation to the rollbar project as
    well.
    
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import rollbar from 'rollbar-browser'
 import React from 'react'
 import ReactDOM from 'react-dom'
 // TODO: register service worker:
@@ -8,6 +9,20 @@ import 'tachyons-forms/css/tachyons-forms.css'
 import './index.styl'
 
 import App from './components/App'
+
+const rollbarConfig = {
+  // Only enable error reporting if user is loading the website via peerpad.net
+  enabled: window.location.hostname === 'peerpad.net',
+  accessToken: '2eaed8c2c5e243af8497d15ea90b407e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: process.env.NODE_ENV
+  }
+}
+const Rollbar = rollbar.init(rollbarConfig)
+window.Rollbar = Rollbar
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
 // registerServiceWorker()
