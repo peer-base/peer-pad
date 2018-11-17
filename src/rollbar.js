95aa27f629f884d1bcbe6d1dda71a5b385529fe7
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-11-17 15:32:42 -0500

    chore: anonomize rollbar data
    - Moved rollbar initialization to it's own file
    - Real change is really the addition of `captureIp: false` and the
    transformer that strips away the encryption key from request.url when
    sent to rollbar
    
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

---
+++ src/rollbar.js
@@ -0,0 +1,32 @@
+import rollbar from 'rollbar'
+
+const rollbarTransformer = (payload) => {
+  // This removes the last part of a URL, in case we're inside a pad, we don't
+  // want to share the encryption key with Rollbar
+  payload.request.url = payload.request.url.split('/').slice(0, -1).join('/')
+}
+
+module.exports = (hostname) => {
+  const rollbarConfig = {
+    // Only enable error reporting if user is loading the website via peerpad.net
+    enabled: hostname === 'peerpad.net',
+    captureIp: false,
+    accessToken: '2eaed8c2c5e243af8497d15ea90b407e',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    transform: rollbarTransformer,
+    payload: {
+      environment: process.env.NODE_ENV,
+      client: {
+        javascript: {
+          source_map_enabled: true,
+          code_version: process.env.GIT_COMMIT,
+          guess_uncaught_frames: true
+        }
+      }
+    }
+  }
+
+  const Rollbar = rollbar.init(rollbarConfig)
+  return Rollbar
+}
