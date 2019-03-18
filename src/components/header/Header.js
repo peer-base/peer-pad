92f54b63163ec55c2e17ce9b0c9cb2ca8bded236
Jim Pick <jim@backblend.com>
2019-03-18 16:18:05 -0700

    UI Tweaks ... use full width, scroll in wide code blocks, hide debugging
    Quick fixes for the next round of testing.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom'
 
 export default ({ children }) => (
   <div className='pa3 bg-big-stone mb4'>
-    <div className='mw8 center'>
-      <div className='flex flex-row items-center'>
-        <Link to='/' data-id='home-link'>
-          <img src='images/logo-peerpad.svg' alt='PeerPad logo' className='mr4' />
-        </Link>
-        {children}
-      </div>
+    <div className='flex flex-row items-center'>
+      <Link to='/' data-id='home-link'>
+        <img src='images/logo-peerpad.svg' alt='PeerPad logo' className='mr4' />
+      </Link>
+      {children}
     </div>
   </div>
 )
